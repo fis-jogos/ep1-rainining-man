6463bbdb72dcb80a0fd40c2cc3ad36627ec86b0e
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-16 11:58:22 -0300

    Change tabs to spaces in run
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- run.py
+++ run.py
@@ -5,22 +5,30 @@ from umbrella import Umbrella
 
 
 def draw():
-	world.draw()
+    world.draw()
 
 def update(dt):
-	screen.clear()
-	world.update(dt)
-			
+    screen.clear()
+    world.update(dt)
+
 # Simulacao
 WIDTH = World.WIDTH = 400
-HEIGHT = World.HEIGHT = 400
+HEIGHT = World.HEIGHT = 900
 
-world = World(gravity=100)
+world = World(gravity=50)
 boyStartX = World.WIDTH/2
 boyStartY = World.HEIGHT
 umbrella = Umbrella(Actor('umbrella'),positionX=boyStartX, positionY=boyStartY)
 boy = Boy(Actor('1'), positionX=boyStartX,positionY=boyStartY, mass=100,
- speed=30, gravity=world.gravity,umbrella=umbrella)
+ speed=0, gravity=world.gravity,umbrella=umbrella)
+
+def on_mouse_down():
+    boy.k = 10
+    boy.actor.image = '2'
+
+def on_mouse_up():
+    boy.k = 1.05
+    boy.actor.image = '1'
 
 world.add(umbrella)
 world.add(boy)
