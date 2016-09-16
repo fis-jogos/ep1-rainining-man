92e0e65d1c5dc2882ccdd2f10f2530eb0a87a54b
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-09-16 11:58:03 -0300

    Adding variables to class
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- boy.py
+++ boy.py
@@ -4,45 +4,39 @@ from world import World
 from umbrella import Umbrella
 
 class Boy:
-	"""
-		This class represents the main party of the body in study
-	"""
-	def __init__(self, actor, positionX, positionY,mass, speed, umbrella, gravity=100):
-		self.actor = actor
-		self.positionX = positionX
-		self.positionY = positionY
-		self.mass = mass
-		self.speed = speed
-		self.acceleration = 0
-		self.umbrella = umbrella
-		self.gravity = gravity
+    """
+        This class represents the main party of the body in study
+    """
+    def __init__(self, actor, positionX, positionY,mass, speed, umbrella, gravity=100):
+        self.actor = actor
+        self.positionX = positionX
+        self.positionY = positionY
+        self.mass = mass
+        self.speed = speed
+        self.acceleration = 0
+        self.umbrella = umbrella
+        self.gravity = gravity
+        self.areaBoy = self.actor.width * self.actor.height
+        self.areaUmbrella = self.umbrella.actor.width * self.umbrella.actor.height
+        self.total_area =  self.areaBoy + self.areaUmbrella
+        self.total_mass = self.mass + self.umbrella.mass
+        self.k = 1.05
 
-	def update(self, dt):
-		areaBoy = self.actor.width * self.actor.height
-		areaUmbrella = self.umbrella.actor.width * self.umbrella.actor.height
-		total_area =  areaBoy + areaUmbrella
-		total_mass = self.mass + self.umbrella.mass
-		k = 1.5
-		resist = k * self.speed ** 2
-		if self.positionY > 300:
-			self.acceleration = -(resist - self.mass * self.gravity) / total_mass
-			self.speed += self.acceleration * dt
-		else:
-			self.actor.image = '2'
-			kp =10
-			self.acceleration = (-(kp * self.speed ** 2) + self.mass * self.gravity)/total_mass
-			self.speed += self.acceleration * dt
+    def update(self, dt):
+        resist = self.k * (self.speed ** 2)
+        self.acceleration = -(resist - self.mass * self.gravity) / self.total_mass
+        self.speed += self.acceleration * dt
 
-		print (self.speed)
-		if self.positionY > 20:
-			self.positionY -= self.speed * dt
-			self.umbrella.positionY = self.positionY + 20
-		else:
-			self.speed = 0
-			self.acceleration = 0
-			self.gravity = 0
+        print (self.speed)
+        if self.positionY > 20:
+            self.positionY -= self.speed * dt
+            self.umbrella.positionY = self.positionY + 20
+        else:
+            self.speed = 0
+            self.acceleration = 0
+            self.gravity = 0
 
-	def draw(self):
-		self.actor.x = self.positionX
-		self.actor.y = World.HEIGHT - self.positionY
-		self.actor.draw() 
+    def draw(self):
+        self.actor.x = self.positionX
+        self.actor.y = World.HEIGHT - self.positionY
+        self.actor.draw()
