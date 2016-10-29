b702434bfdac14dfdded5a1965a04cc69fc8c976
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2016-10-29 11:42:17 -0200

    Updates made by Tiago and Gustavo on https://github.com/TiagoAssuncao/raining-man

--- boy.py
+++ boy.py
@@ -3,18 +3,8 @@ from FGAme import *
 class Boy:
 
     def __init__(self):
-        self.body = world.add.aabb(shape=(50,80), pos=(400, 560), mass=100)
+        self.body = world.add.aabb(shape=(30,80), pos=(400, 500), mass=2000)
         self.k = 1.05
 
     def update(self):
-        x, y = self.body.vel
-        drag = self.k * (y ** 2)
-        a = (self.body.mass * self.body.gravity[1] + drag) / self.body.mass
-        speed_x, speed_y = self.body.vel
-        speed_y += a * 1/60
-        pos_x, pos_y = self.body.pos
-        pos_y -= speed_y * 1/60
-        self.body.vel = (speed_x, speed_y)
-
-        if(self.body.pos[1] < 35):
-            self.body.vel = (0, 0)
+        pass
--- run.py
+++ run.py
@@ -1,37 +1,91 @@
 from FGAme import *
 from math import sqrt
+import pygame
 from boy import *
 from shot import Shot
+import random
+import time
 
-world.add.margin(10, -10)
-world.gravity = (0, -10)
-player = Boy()
-player.body.gravity = world.gravity
-gravity_x, gravity_y = player.body.gravity
-gambiarra = True
+world.add.margin(200, -500)
+world.gravity = (0, 30)
+PLAYER = Boy()
+PLAYER.body.gravity = (0, 0)
+gravity_x, gravity_y = PLAYER.body.gravity
+
+shot_list = []
+shot = Shot() # INITIAL SHOT
+shot_list.append(shot)
+count = 0
+
+pygame.mixer.pre_init(44100, 16, 2, 4096)
+pygame.init()
+main_sound = pygame.mixer.music.load("sound-adventure.mp3")
+main_sound = pygame.mixer.music.play()
+pygame.mixer.music.set_volume(0.8);
+
+
+def randomize():
+    for i in range(random.randint(1, 2)):
+        shot = Shot()
+        global count
+        count = count + 1
+        randomizer = random.randint(-100, 100)
+        shot.x = randomizer + random.randint(300, 500)
+        shot.y = shot.y + random.randint(-70, -50) # TO NOT GET TOGETHER
+        shot_list.append(shot)
+        shot.vel = (0, 10 + 10*count)
 
 @listen('long-press', 'up')
 def increase_drag():
-    player.k = 10
+    for shot in shot_list:
+        shot.k = 100    
+
+@listen('long-press', 'left', dx=-2)
+@listen('long-press', 'right', dx=2)
+def move_p1(dx):
+        PLAYER.body.move(dx, 0)
 
 @listen('key-up', 'up')
 def decrease_drag():
-    player.k = 1.05
-
+    for shot in shot_list:
+        shot.k = 1.05
 
 @listen('key-down', 'x')
 def exit_game():
     exit()
 
+
+timer = 0 # GAME TIMER
+
+@listen ('frame-enter')
 def update():
-    global gambiarra
-    player.update()
-    if gambiarra:
-    	shot = Shot()
-    	world.add(shot)
-    	gambiarra = False
+    global timer
 
-#world.add(player)
+    if timer >= 100: # TIME TO SPAWN
+        timer = 0
+        randomize()
 
+    for shot in shot_list:
+        shot.update()
+
+        if shot.pos[1] > 600:
+            shot_list.remove(shot)
+            world.remove(shot)
+
+    # END GAME
+    if PLAYER.body.vel[1] > 0:
+        print("GAME OVER")
+        dash_sound =  pygame.mixer.music.load("battle_theme.mp3")
+        dash_sound = pygame.mixer.music.play()
+        time.sleep(1)
+        exit()
+
+    # TIMER INCREASING DEPENDS ON K VALUE
+    if not shot_list:
+        timer += 1
+    elif (shot_list[0].k) == 0.1:
+        timer += 1
+    else: # SLOW DOWN THE TIME
+        timer += 0.5
 
 run()
